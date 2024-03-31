d7653b756e3d2f6750730804ea88e6b17dfb9487
Andre <722111+AndreSantos@users.noreply.github.com>
2024-03-31 22:51:21 +0100

    Read DB from folder outside of project directory.

--- src/db/db.js
+++ src/db/db.js
@@ -2,16 +2,16 @@ import { readFileSync, writeFileSync } from 'node:fs';
 
 export function persistData(itemsCache, itemsRead) {
     persistItemsCache(itemsCache);
-	writeFileSync('dump/items_read.txt', JSON.stringify(itemsRead));
+	writeFileSync('../../dump/items_read.txt', JSON.stringify(itemsRead));
 }
 
 export function persistIterations(iterations) {
-	writeFileSync('dump/iterations.txt', JSON.stringify(iterations));
+	writeFileSync('../../dump/iterations.txt', JSON.stringify(iterations));
 }
 
 export function persistItemsCache(itemsCache) {
-    const unwantedSets = JSON.parse(readFileSync('dump/unwanted_sets.txt', 'utf8'));
-    const unwantedItems = JSON.parse(readFileSync('dump/unwanted_items.txt', 'utf8'));
+    const unwantedSets = JSON.parse(readFileSync('../../dump/unwanted_sets.txt', 'utf8'));
+    const unwantedItems = JSON.parse(readFileSync('../../dump/unwanted_items.txt', 'utf8'));
     const items = {};
     Object.keys(itemsCache).filter(key => !unwantedSets[key]).forEach(key => {
         const filteredItems = itemsCache[key].filter(item => !unwantedItems[item.id]);
@@ -19,36 +19,36 @@ export function persistItemsCache(itemsCache) {
             items[key] = filteredItems;
         }
     });
-    writeFileSync('dump/items.txt', JSON.stringify(items));
+    writeFileSync('../../dump/items.txt', JSON.stringify(items));
 }
 
 export function persistPrices(prices) {
-	writeFileSync('dump/prices.txt', JSON.stringify(prices));
+	writeFileSync('../../dump/prices.txt', JSON.stringify(prices));
 }
 
 export function persistUnwantedItems(unwantedItems) {
-    writeFileSync('dump/unwanted_items.txt', JSON.stringify(unwantedItems));
+    writeFileSync('../../dump/unwanted_items.txt', JSON.stringify(unwantedItems));
 }
 
 export function persistUnwantedSets(unwantedSets) {
-    writeFileSync('dump/unwanted_sets.txt', JSON.stringify(unwantedSets));
+    writeFileSync('../../dump/unwanted_sets.txt', JSON.stringify(unwantedSets));
 }
 
 export function readData() {
-    const itemsCache = JSON.parse(readFileSync('dump/items.txt', 'utf8'));
-    const itemsRead = JSON.parse(readFileSync('dump/items_read.txt', 'utf8'));
+    const itemsCache = JSON.parse(readFileSync('../../dump/items.txt', 'utf8'));
+    const itemsRead = JSON.parse(readFileSync('../../dump/items_read.txt', 'utf8'));
     const iterations = readIterations();
-    const prices = JSON.parse(readFileSync('dump/prices.txt', 'utf8'));
-    const unwantedSets = JSON.parse(readFileSync('dump/unwanted_sets.txt', 'utf8'));
+    const prices = JSON.parse(readFileSync('../../dump/prices.txt', 'utf8'));
+    const unwantedSets = JSON.parse(readFileSync('../../dump/unwanted_sets.txt', 'utf8'));
     let unwantedItems;
     try {
-        unwantedItems = JSON.parse(readFileSync('dump/unwanted_items.txt', 'utf8'));
+        unwantedItems = JSON.parse(readFileSync('../../dump/unwanted_items.txt', 'utf8'));
     } catch (e) { unwantedItems = {};}
-    const unwantedUsers = JSON.parse(readFileSync('dump/unwanted_users.txt', 'utf8'));
+    const unwantedUsers = JSON.parse(readFileSync('../../dump/unwanted_users.txt', 'utf8'));
     return {itemsCache, itemsRead, iterations, prices, unwantedSets, unwantedItems, unwantedUsers};
 }
 
 export function readIterations() {
-    const iterations = JSON.parse(readFileSync('dump/iterations.txt', 'utf8'));
+    const iterations = JSON.parse(readFileSync('../../dump/iterations.txt', 'utf8'));
     return iterations;
 }
