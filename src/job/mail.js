b1b46d17edb925f1a19c18f3d6973d8e28af492c
Andre <722111+AndreSantos@users.noreply.github.com>
2024-07-07 16:43:49 +0100

    Improve price alert email for new condition and pack

--- src/job/mail.js
+++ src/job/mail.js
@@ -22,27 +22,51 @@ function initTransporter() {
   }
 }
 
-export function sendMail(set, item, minPrice) {
+function vintedLink(sets) {
+  return sets.split(' + ').map(set => `<li><a href="https://www.vinted.pt/catalog?search_text=lego%20${set}&order=price_low_to_high">Sets in vinted</a></li>`).join('');
+}
+
+function bricklinkLink(sets) {
+  return sets.split(' + ').map(set => `<li><a href="https://www.bricklink.com/v2/catalog/catalogitem.page?S=${set}-1#O={"ii":0}"'>Bricklink</a></li>`).join('');
+}
+
+function bricklinkPics(sets) {
+  return sets.split(' + ').map(set => `<img width="400" src="https://img.bricklink.com/ItemImage/SN/0/${set}-1.png" />`).join('');
+}
+
+function itemPics(item) {
+  return item.photos.map(pic => `<img width="400" src="${pic}" />`).join('');
+}
+
+export function sendMail(sets, item, minPrice) {
   initTransporter();
 
   const email = getEmailCredentials().user;
+  
+  const isNewPrefix = item.status.startsWith("Novo") ? 'New ' : '';
+  const isPackPrefix = sets.includes('+') ? 'Pack ' : '';
+  let titlePrefix = `${isNewPrefix}${isPackPrefix}`;
+  titlePrefix = titlePrefix.length > 0 ? ` (${titlePrefix})`: titlePrefix;
+
 
   var mailOptions = {
     from: email,
     to: email,
-    subject: `Price alert: Set ${set} price: ${item.price}€ (${minPrice}€), discount: ${item.discount}`,
+    subject: `Price alert${titlePrefix}: Set ${sets} price: ${item.price}€ (${minPrice}€)`,
     text: ``,
     html: `
       <ul>
         <li><a href="${item.url}">Item</a></li>
-        <li><a href="https://www.vinted.pt/catalog?search_text=lego%20${set}&order=price_low_to_high">Sets in vinted</a></li>
-        <li><a href="https://www.bricklink.com/v2/catalog/catalogitem.page?S=${set}-1#O={"ii":0}"'>Bricklink</a></li>
+        ${vintedLink(sets)}
+        ${bricklinkLink(sets)}
         <li>Title: ${item.title}</li>
         <li>Description: ${item.description}</li>
+        <li>Condition: ${item.status}</li>
+        <li>User discount: ${item.discount}</li>
       </ul>
-      <img width="400" src="https://img.bricklink.com/ItemImage/SN/0/${set}-1.png" />
+      ${bricklinkPics(sets)}
       <hr />
-      <img width="400" src="${item.photos[0]}" />
+      ${itemPics(item)}
     `
   };
 
