42e6bd37764913512afe4ee222ebd6e9c12dc36d
Andre <722111+AndreSantos@users.noreply.github.com>
2025-01-04 14:57:25 +0000

    Fix cookie name  in /view call.

--- src/vinted/view_item.js
+++ src/vinted/view_item.js
@@ -13,7 +13,7 @@ export async function viewItem(item) {
   const vintedResponse = await fetch(url, {
     signal: controller.signal,
     headers: {
-        cookie: '_vinted_fr_session=' + cookie,
+        cookie: 'access_token_web=' + cookie,
         'user-agent': new UserAgent().toString(),
         accept: 'application/json, text/plain, */*'
     }
